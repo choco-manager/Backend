1b8fa22f7a33ec278e95cfd5a8e6dc5d5865fc79
Daniil <dadyarri@gmail.com>
2023-06-02 15:38:11 +0300

    feat(db): add model for client

---
+++ Backend/Modules/Clients/Contract/Client.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Data;
+using Backend.Modules.Addresses.Contract;
+
+namespace Backend.Modules.Clients.Contract;
+
+public class Client: BaseModel
+{
+    public required string FirstName { get; set; }
+    public required string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? ChatLink { set; get; }
+    public required List<Address> Addresses { get; set; }
+}

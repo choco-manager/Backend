70148c2c12f3fdc6b9427ce9a4fd015d6cad79ea
Daniil <dadyarri@gmail.com>
2023-06-02 17:37:20 +0300

    feat(api): add module for city with some endpoints

---
+++ Backend/Modules/Cities/CitiesModule.cs
@@ -0,0 +1,39 @@
+﻿using Backend.Data;
+using Backend.Modules.Cities.Contract;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Modules.Cities;
+
+public class CitiesModule : IModule
+{
+    public IServiceCollection RegisterModule(IServiceCollection builder)
+    {
+        builder.AddSingleton<AbstractValidator<City>, CityValidator>();
+        return builder;
+    }
+
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet(
+            "/api/cities",
+            async (ApplicationDbContext db) => TypedResults.Ok(await db.Cities.ToListAsync())
+        );
+
+        endpoints.MapPost("/api/cities", CreateCityHandler);
+
+        return endpoints;
+    }
+
+    private async Task<IResult> CreateCityHandler([FromBody] City city, ApplicationDbContext db,
+        AbstractValidator<City> validator)
+    {
+        var result = await validator.ValidateAsync(city);
+
+        if (!result.IsValid) return TypedResults.BadRequest(result.Errors);
+
+        await db.Cities.AddAsync(city);
+        return TypedResults.Created("/api/cities");
+    }
+}

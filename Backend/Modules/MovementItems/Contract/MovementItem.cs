164a1c75ede661857d5ca5129f777b32a1efad45
Daniil <dadyarri@gmail.com>
2023-06-02 15:06:17 +0300

    feat(db): add model for movement item

---
+++ Backend/Modules/MovementItems/Contract/MovementItem.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Data;
+using Backend.Modules.Products.Contract;
+
+namespace Backend.Modules.MovementItems.Contract;
+
+public class MovementItem: BaseModel
+{
+    public required Product Product { get; set; }
+    public decimal Amount { get; set; }
+}

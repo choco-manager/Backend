fc2fd48794e5bb41871ccdbac535aea54c080818
Daniil <dadyarri@gmail.com>
2023-06-02 15:12:21 +0300

    feat(db): add model for address

---
+++ Backend/Modules/Addresses/Contract/Address.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Data;
+using Backend.Modules.Cities.Contract;
+
+namespace Backend.Modules.Addresses.Contract;
+
+public class Address: BaseModel
+{
+    public required City City { get; set; }
+    public required string Street { get; set; }
+    public required string Building { get; set; }
+    
+}

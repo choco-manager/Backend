1c7810261b5e88b65e0a40e0ba328bc4ceed22ed
Daniil <dadyarri@gmail.com>
2023-06-02 14:30:31 +0300

    feat(api): add base interface for modules and extensions to register modules automatically

---
+++ Backend/Modules/IModule.cs
@@ -0,0 +1,42 @@
+﻿namespace Backend.Modules;
+
+public interface IModule
+{
+    IServiceCollection RegisterModule(IServiceCollection builder);
+    IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+}
+ 
+public static class ModuleExtensions
+{
+    private static readonly List<IModule> RegisteredModules = new();
+ 
+    public static IServiceCollection RegisterModules(this IServiceCollection services)
+    {
+        var modules = DiscoverModules();
+        foreach (var module in modules)
+        {
+            module.RegisterModule(services);
+            RegisteredModules.Add(module);
+        }
+ 
+        return services;
+    }
+ 
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        foreach (var module in RegisteredModules)
+        {
+            module.MapEndpoints(app);
+        }
+        return app;
+    }
+ 
+    private static IEnumerable<IModule> DiscoverModules()
+    {
+        return typeof(IModule).Assembly
+            .GetTypes()
+            .Where(p => p.IsClass && p.IsAssignableTo(typeof(IModule)))
+            .Select(Activator.CreateInstance)
+            .Cast<IModule>();
+    }
+}

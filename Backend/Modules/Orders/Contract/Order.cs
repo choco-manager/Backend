01184f16feca2be108991c00de8ee0d168980bc5
Daniil <dadyarri@gmail.com>
2023-06-02 15:48:12 +0300

    feat(db): add model for orders

---
+++ Backend/Modules/Orders/Contract/Order.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Data;
+using Backend.Modules.Addresses.Contract;
+using Backend.Modules.Clients.Contract;
+using Backend.Modules.MovementItems.Contract;
+using Backend.Modules.MovementStatuses.Contract;
+
+namespace Backend.Modules.Orders.Contract;
+
+public class Order: BaseModel
+{
+    public DateOnly Date { get; set; }
+    public required MovementStatus Status { get; set; }
+    public required List<MovementItem> Items { get; set; }
+    public required Client Client { get; set; }
+    public required Address SelectedAddress { get; set; }
+    public bool IsDeleted { get; set; }
+}

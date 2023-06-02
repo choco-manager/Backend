0d6d769d080a89f750c2117afe4379a38d3ac99a
Daniil <dadyarri@gmail.com>
2023-06-02 15:22:32 +0300

    feat(db): add model for shipment

---
+++ Backend/Modules/Shipments/Contract/Shipment.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Data;
+using Backend.Modules.MovementItems.Contract;
+using Backend.Modules.MovementStatuses.Contract;
+
+namespace Backend.Modules.Shipments.Contract;
+
+public class Shipment: BaseModel
+{
+    public DateOnly Date { get; set; }
+    public required MovementStatus Status { get; set; }
+    public required List<MovementItem> Items { get; set; }
+    public bool IsDeleted { get; set; }
+}

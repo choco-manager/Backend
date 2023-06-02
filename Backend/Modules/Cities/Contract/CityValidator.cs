733256049b6fddeb12f9b6eff86752a827eb3ec4
Daniil <dadyarri@gmail.com>
2023-06-02 17:35:24 +0300

    feat(data): add validator for city model

---
+++ Backend/Modules/Cities/Contract/CityValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Backend.Modules.Cities.Contract;
+
+public class CityValidator: AbstractValidator<City>
+{
+    public CityValidator()
+    {
+        RuleFor(city => city.Name).NotEmpty().NotNull();
+    }
+}

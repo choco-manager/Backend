841c862bb5685b777fa274d1497972353d8567bb
Daniil <dadyarri@gmail.com>
2023-06-04 01:32:06 +0300

    feat(tests): add shared fixture for modules tests

---
+++ Backend.Tests.Unit/Modules/ModulesTestsSetup.cs
@@ -0,0 +1,38 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+using NUnit.Framework;
+
+
+namespace Backend.Tests.Unit.Modules;
+
+[SetUpFixture]
+public class ModulesTestsSetup {
+  public static AppFactory Factory;
+  private IContainer _pgContainer;
+
+  [OneTimeSetUp]
+  public async Task CreateContainer() {
+    const string postgresPwd = "pgpwd";
+
+    _pgContainer = new ContainerBuilder()
+      .WithName(Guid.NewGuid().ToString("N"))
+      .WithImage("postgres:15")
+      .WithHostname("chocoTest")
+      .WithExposedPort(5432)
+      .WithPortBinding(5432, true)
+      .WithEnvironment("POSTGRES_PASSWORD", postgresPwd)
+      .WithEnvironment("PGDATA", "/pgdata")
+      .WithTmpfsMount("/pgdata")
+      .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("psql -U postgres -c \"select 1\""))
+      .Build();
+    await _pgContainer.StartAsync();
+
+    Factory = new(_pgContainer.Hostname, _pgContainer.GetMappedPublicPort(5432), postgresPwd);
+  }
+  
+  [OneTimeTearDown]
+  public async Task DisposeContainer() {
+    await _pgContainer.DisposeAsync();
+  }
+}

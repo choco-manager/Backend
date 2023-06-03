a5a332af9b2fd30f9dc091a4576acfefb493bff7
Daniil <dadyarri@gmail.com>
2023-06-03 20:30:48 +0300

    feat(api): add configuration for mapperly

---
+++ Backend/Data/Mappers.cs
@@ -0,0 +1,10 @@
+﻿using Backend.Modules.Cities.Contract;
+using Riok.Mapperly.Abstractions;
+
+namespace Backend.Data;
+
+[Mapper]
+public partial class Mappers
+{
+    public partial City Map(CityRequestBody city);
+}

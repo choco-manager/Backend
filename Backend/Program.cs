aa25b370ec39c37b11210e9b366b6348c414d11f
Daniil <dadyarri@gmail.com>
2023-06-03 15:01:10 +0300

    feat(config): configure serilog

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,9 +1,18 @@
 using Backend.Data;
 using Backend.Modules;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Default", LogEventLevel.Debug)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 builder.Services.RegisterModules();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapEndpoints();

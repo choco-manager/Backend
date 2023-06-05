89584cddcc3b2ae699d77fcc245bb2a0e9ee5ed8
Daniil <dadyarri@gmail.com>
2023-06-05 23:40:42 +0300

    feat(data): add helper for generating fake addresses and putting them to database

---
+++ Backend/Data/FakeDataGeneration/GenerateAddresses.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Modules.Addresses.Contract;
+
+using Bogus;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Backend.Data.FakeDataGeneration; 
+
+public class GenerateAddresses {
+  
+  public GenerateAddresses() {
+  }
+
+  public async Task<List<Address>> Generate(ApplicationDbContext context) {
+    var cities = await context.Cities.ToListAsync();
+
+    var addresses = new Faker<Address>("ru")
+      .StrictMode(true)
+      .RuleFor(a => a.Id, f => Guid.NewGuid())
+      .RuleFor(a => a.City, f => f.PickRandom(cities))
+      .RuleFor(a => a.Street, f => f.Address.StreetName())
+      .RuleFor(a => a.Building, f => f.Address.BuildingNumber());
+
+    var generated = addresses.GenerateBetween(10, 20);
+    await context.Addresses.AddRangeAsync(generated);
+    await context.SaveChangesAsync();
+
+    return generated;
+  }
+}

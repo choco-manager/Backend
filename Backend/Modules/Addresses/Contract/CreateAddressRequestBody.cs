fd1b660fa81e244bc91f350fa15cb29de870ea10
Daniil <dadyarri@gmail.com>
2023-06-05 23:40:17 +0300

    feat(api): add request body for creating address and validator for it

---
+++ Backend/Modules/Addresses/Contract/CreateAddressRequestBody.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Modules.Addresses.Contract; 
+
+public class CreateAddressRequestBody {
+  public Guid City { get; set; }
+  public required string Street { get; set; }
+  public required string Building { get; set; }
+}
---
+++ Backend/Modules/Addresses/Contract/CreateAddressRequestBodyValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+
+namespace Backend.Modules.Addresses.Contract;
+
+public class CreateAddressRequestBodyValidator : AbstractValidator<CreateAddressRequestBody> {
+  public CreateAddressRequestBodyValidator() {
+    RuleFor(address => address.City).NotEmpty().NotEqual(Guid.Empty).NotNull();
+    RuleFor(address => address.Street).NotEmpty().NotNull();
+    RuleFor(address => address.Building).NotEmpty().NotNull();
+  }
+}

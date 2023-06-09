e9597004c771063207025d495d6e8a38b7b27324
Daniil <dadyarri@gmail.com>
2023-06-09 19:06:18 +0300

    feat(api): hide some fields from price change item

--- Backend/Modules/PriceChanges/Contract/PriceChange.cs
+++ Backend/Modules/PriceChanges/Contract/PriceChange.cs
@@ -19,6 +19,8 @@
 
 #region
 
+using System.Text.Json.Serialization;
+
 using Backend.Data;
 using Backend.Modules.PriceTypes.Contract;
 using Backend.Modules.Products.Contract;
@@ -29,8 +31,14 @@ using Backend.Modules.Products.Contract;
 namespace Backend.Modules.PriceChanges.Contract;
 
 public class PriceChange : BaseModel {
-  public required Product Product { get; set; }
-  public required PriceType PriceType { get; set; }
+  [JsonIgnore]
+  public override Guid Id { get; set; }
+
+  [JsonIgnore]
+  public Product Product { get; set; }
+
+  [JsonIgnore]
+  public PriceType PriceType { get; set; }
   public DateTime ChangeTimestamp { get; set; }
   public int Price { get; set; }
 }

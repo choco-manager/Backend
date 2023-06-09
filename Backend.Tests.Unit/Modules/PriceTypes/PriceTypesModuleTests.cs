ba5ee3b382ab37d06b6252bfb76c30a7bef17033
Daniil <dadyarri@gmail.com>
2023-06-09 11:58:48 +0300

    feat(tests): add price types api tests

---
+++ Backend.Tests.Unit/Modules/PriceTypes/PriceTypesModuleTests.cs
@@ -0,0 +1,70 @@
+﻿// ------------------------------------------------------------------------
+// Copyright (C) 2023 dadyarri
+// This file is part of ChocoManager <https://github.com/choco-manager/Backend>.
+// 
+// ChocoManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ChocoManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ChocoManager.  If not, see <http://www.gnu.org/licenses/>.
+// ------------------------------------------------------------------------
+// 
+
+
+#region
+
+using System.Net;
+using System.Net.Http.Json;
+
+using Backend.Modules.PriceTypes.Contract;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using NUnit.Framework;
+
+#endregion
+
+
+namespace Backend.Tests.Unit.Modules.PriceTypes;
+
+[TestFixture]
+public class PriceTypesModuleTests {
+  private HttpClient _client = null!;
+  private IServiceScope _scope = null!;
+
+
+  [SetUp]
+  public void Setup() {
+    _scope = ModulesTestsSetup.Factory.Services.CreateScope();
+    _client = ModulesTestsSetup.Factory.CreateClient();
+  }
+
+  [TearDown]
+  public void TearDown() {
+    _scope.Dispose();
+    _client.Dispose();
+  }
+
+
+  [Test(Description =
+    "Method to get list of price types should return status code 200 and list from 5 predefined price types")]
+  public async Task GetPriceTypes__Returns200() {
+    // Act
+    var response = await _client.GetAsync("/api/priceTypes");
+    var movementStatuses = await response.Content.ReadFromJsonAsync<List<PriceType>>();
+
+    // Assert
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+    movementStatuses.Should().NotBeNull();
+    movementStatuses?.Count.Should().Be(2);
+  }
+}

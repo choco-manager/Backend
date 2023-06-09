bc917f0f7059bb6bfac71fb7a9dca361995bee57
Daniil <dadyarri@gmail.com>
2023-06-09 19:06:41 +0300

    feat(api): add enumeration with ids of price types

---
+++ Backend/Modules/PriceTypes/Contract/PriceTypeEnum.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------------------
+// Copyright (C) 2023 dadyarri
+// This file is part of ChocoManager <https://github.com/choco-manager/Backend>.
+// 
+// ChocoManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ChocoManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ChocoManager.  If not, see <http://www.gnu.org/licenses/>.
+// ------------------------------------------------------------------------
+// 
+
+
+namespace Backend.Modules.PriceTypes.Contract;
+
+public static class PriceTypeEnum {
+  public static readonly Guid Retail = new("0d501699-abaf-42ad-a858-eaacc5c50199");
+  public static readonly Guid Wholesale = new("3165c0b3-f92f-4a2d-8278-d131b8a32478");
+}

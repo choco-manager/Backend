9a9ddfa52d0d8ad39c675addddf7e04c7e0e73b9
Daniil <dadyarri@gmail.com>
2023-06-09 18:10:48 +0300

    feat(db): add prices to products

--- Backend/Modules/Products/Contract/Product.cs
+++ Backend/Modules/Products/Contract/Product.cs
@@ -30,6 +30,8 @@ namespace Backend.Modules.Products.Contract;
 public class Product : BaseModel {
   public required string Name { get; set; }
   public required ProductCategory Category { get; set; }
+  public required int WholesalePrice { get; set; }
+  public required int RetailPrice { get; set; }
   public bool IsByWeight { get; set; }
   public bool IsDeleted { get; set; }
   public int VkMarketId { get; set; }

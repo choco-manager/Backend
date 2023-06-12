9f70c0fdf81b1b26f6cf2a280f05f14fd30314d4
Daniil <dadyarri@gmail.com>
2023-06-12 12:33:37 +0300

    feat(api): add validator for update client request body

---
+++ Backend/Modules/Clients/Contract/UpdateClientRequestBodyValidator.cs
@@ -0,0 +1,38 @@
+﻿// ------------------------------------------------------------------------
+// Copyright (C) 2023 dadyarri
+// This file is part of ChocoManager <https://github.com/choco-manager/Backend>.
+// 
+// ChocoManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ChocoManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ChocoManager.  If not, see <http://www.gnu.org/licenses/>.
+// ------------------------------------------------------------------------
+// 
+
+
+#region
+
+using FluentValidation;
+
+#endregion
+
+
+namespace Backend.Modules.Clients.Contract;
+
+public class UpdateClientRequestBodyValidator : AbstractValidator<UpdateClientRequestBody> {
+  public UpdateClientRequestBodyValidator() {
+    RuleFor(b => b.FirstName).NotEmpty().NotNull();
+    RuleFor(b => b.LastName).NotEmpty().NotNull();
+    RuleFor(b => b.ChatLink)
+      .Matches("https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)");
+    RuleForEach(b => b.Addresses).NotEmpty().NotNull().NotEqual(Guid.Empty);
+  }
+}

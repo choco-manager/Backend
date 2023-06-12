1685fdb07fdb3399f09eff0629adfd48478e2891
Daniil <dadyarri@gmail.com>
2023-06-12 12:15:02 +0300

    feat(tests): add tests for creating client and comparing delta of addresses

--- Backend.Tests.Unit/Modules/Clients/ClientsModuleTests.cs
+++ Backend.Tests.Unit/Modules/Clients/ClientsModuleTests.cs
@@ -23,6 +23,7 @@
 using System.Net;
 using System.Net.Http.Json;
 
+using Backend.Modules.Clients;
 using Backend.Modules.Clients.Contract;
 
 using FluentAssertions;
@@ -78,5 +79,74 @@ public class ClientsModuleTests {
     response.StatusCode.Should().Be(HttpStatusCode.OK);
     addresses.Should().NotBeNull();
     addresses?.Count.Should().Be(5);
+  }
+
+  [Test(Description = "Method to create new client should return status code 201 and created client")]
+  public async Task CreateClient__Returns201() {
+    // Act
+    var response = await _client.PostAsync("/api/clients", JsonContent.Create(new UpdateClientRequestBody {
+      FirstName = "John",
+      LastName = "Doe",
+      ChatLink = "https://vk.com/gim1234567_1",
+      PhoneNumber = "(678) 673-34-23",
+      Addresses = new List<Guid>(),
+    }));
+
+    var client = await response.Content.ReadFromJsonAsync<ClientDto>();
+
+    // Assert
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+    client.Should().NotBeNull();
+  }
+
+  [Test(Description = "Method to compare two lists of addresses should produce one list with delta of both lists")]
+  public async Task CompareAddresses__WhenListsWasNotChanged__ReturnsSameList() {
+    // Arrange
+    var list = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+
+    // Act
+    var result = ClientsModule.CalculateDeltaOfAddresses(list, list);
+
+    // Assert
+    result.Should().BeEquivalentTo(list);
+  }
+
+  [Test(Description = "Method to compare two lists of addresses should produce one list with delta of both lists")]
+  public async Task CompareAddresses__WhenFirstListHasItemsThatSecondHasnt__CorrectDelta() {
+    // Arrange
+    var firstList = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+    var secondList = new List<Guid>() { firstList[0], firstList[1] };
+
+    // Act
+    var result = ClientsModule.CalculateDeltaOfAddresses(firstList, secondList);
+
+    // Assert
+    result.Should().BeEquivalentTo(secondList);
+  }
+
+  [Test(Description = "Method to compare two lists of addresses should produce one list with delta of both lists")]
+  public async Task CompareAddresses__WhenSecondListHasItemsThatFirstHasnt__ReturnsCorrectDelta() {
+    // Arrange
+    var firstList = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+    var secondList = new List<Guid>() { firstList[0], firstList[1], firstList[2], Guid.NewGuid() };
+
+    // Act
+    var result = ClientsModule.CalculateDeltaOfAddresses(firstList, secondList);
+
+    // Assert
+    result.Should().BeEquivalentTo(secondList);
+  }
+
+  [Test(Description = "Method to compare two lists of addresses should produce one list with delta of both lists")]
+  public async Task CompareAddresses__WhenBothListsHasItemsThatOtherHasnt__ReturnsCorrectDelta() {
+    // Arrange
+    var firstList = new List<Guid>() { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+    var secondList = new List<Guid>() { firstList[0], firstList[1], Guid.NewGuid() };
+
+    // Act
+    var result = ClientsModule.CalculateDeltaOfAddresses(firstList, secondList);
+
+    // Assert
+    result.Should().BeEquivalentTo(secondList);
   }
 }

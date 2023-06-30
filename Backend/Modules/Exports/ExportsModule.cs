76e8bea5217b3c00c6b3dbe83b84fad76d62c161
Daniil <dadyarri@gmail.com>
2023-07-01 00:50:27 +0300

    feat(api): add empty api boilerplate for image export functionality

---
+++ Backend/Modules/Exports/ExportsModule.cs
@@ -0,0 +1,48 @@
+﻿// ------------------------------------------------------------------------
+// Copyright (C) 2023 dadyarri
+// This file is part of ChocoManager <https://github.com/choco-manager/Backend>.
+// 
+// ChocoManager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ChocoManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ChocoManager.  If not, see <http://www.gnu.org/licenses/>.
+// ------------------------------------------------------------------------
+// 
+
+
+#region
+
+using Backend.Data;
+
+using Microsoft.AspNetCore.Mvc;
+
+#endregion
+
+
+namespace Backend.Modules.Exports;
+
+public class ExportsModule : IModule {
+  public IServiceCollection RegisterModule(IServiceCollection builder) {
+    return builder;
+  }
+
+  public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints) {
+    var module = endpoints.MapGroup("/api/export").WithTags("Export API");
+
+    module.MapGet("image", ExportImage);
+
+    return endpoints;
+  }
+
+  private async Task<IResult> ExportImage([FromServices] ApplicationDbContext db) {
+    return TypedResults.Ok();
+  }
+}

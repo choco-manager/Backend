442b32c4be5324b329d0228b4187de5cb7cd94a3
Daniil <dadyarri@gmail.com>
2024-05-04 14:11:17 +0300

    feat: replace useless extensions with own code

---
+++ Api/Extensions/FluentValidationFailuresExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.Result;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Api.Extensions;
+
+public static class FluentValidationFailuresExtensions
+{
+    public static List<ValidationError> AsErrors(this List<ValidationFailure> failures)
+    {
+        List<ValidationError> validationErrorList = [];
+        foreach (var error in failures)
+            validationErrorList.Add(new ValidationError
+            {
+                Severity = FromSeverity(error.Severity),
+                ErrorMessage = error.ErrorMessage,
+                ErrorCode = error.ErrorCode,
+                Identifier = error.PropertyName
+            });
+        return validationErrorList;
+    }
+
+    private static ValidationSeverity FromSeverity(Severity severity)
+    {
+        return severity switch
+        {
+            Severity.Error => ValidationSeverity.Error,
+            Severity.Warning => ValidationSeverity.Warning,
+            Severity.Info => ValidationSeverity.Info,
+            _ => throw new ArgumentOutOfRangeException(nameof(severity), "Unexpected Severity")
+        };
+    }
+}

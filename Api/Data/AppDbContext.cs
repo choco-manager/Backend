9326d1c4d1e571758088ff3688ddfe623ec4d813
Daniil <dadyarri@gmail.com>
2024-05-05 00:22:34 +0300

    db: add table to store user's fcm tokens

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -8,4 +8,5 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public virtual DbSet<User> Users => Set<User>();
     public virtual DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
     public virtual DbSet<RevokedAccessToken> RevokedAccessTokens => Set<RevokedAccessToken>();
+    public virtual DbSet<FcmToken> FcmTokens => Set<FcmToken>();
 }

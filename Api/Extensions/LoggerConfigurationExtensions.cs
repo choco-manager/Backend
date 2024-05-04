9b8138d7721bf57aeb0ffbf67c2697eb1b46a6c0
Daniil <dadyarri@gmail.com>
2024-05-04 13:10:15 +0300

    refactor: extract logger configuration to extension method

---
+++ Api/Extensions/LoggerConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.OpenTelemetry;
+
+namespace Api.Extensions;
+
+public static class LoggerConfigurationExtensions
+{
+    public static LoggerConfiguration Configure(this LoggerConfiguration configuration)
+    {
+        configuration
+            .WriteTo.Console()
+            .WriteTo.OpenTelemetry(opts =>
+            {
+                opts.Endpoint = "http://localhost:4317";
+                opts.Protocol = OtlpProtocol.Grpc;
+                opts.ResourceAttributes = new Dictionary<string, object>
+                {
+                    ["service.name"] = "ChocoManager Main API",
+                };
+            })
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Default", LogEventLevel.Debug)
+            .MinimumLevel
+            .Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Verbose);
+
+        return configuration;
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -2,23 +2,9 @@ using Api.Extensions;
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.OpenTelemetry;
 
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.OpenTelemetry(opts =>
-    {
-        opts.Endpoint = "http://localhost:4317";
-        opts.Protocol = OtlpProtocol.Grpc;
-        opts.ResourceAttributes = new Dictionary<string, object>
-        {
-            ["service.name"] = "ChocoManager Main API",
-        };
-    })
-    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-    .MinimumLevel.Override("Default", LogEventLevel.Debug)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Verbose)
+    .Configure()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder();

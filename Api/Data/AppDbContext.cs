92920bca2d2f02649e152538f3728f8efc577cc2
Daniil <dadyarri@gmail.com>
2024-05-04 21:43:12 +0300

    db: add table for storing revoked access tokens

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -3,8 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public virtual DbSet<User> Users => Set<User>();
     public virtual DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+    public virtual DbSet<RevokedAccessToken> RevokedAccessTokens => Set<RevokedAccessToken>();
 }
---
+++ Api/Data/Models/RevokedAccessToken.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Data.Models;
+
+public class RevokedAccessToken
+{
+    [Key]
+    public required string Token { get; set; }
+}

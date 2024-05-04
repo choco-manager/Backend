dc126888a632e734821d31e6ff930a5bdc661ff1
Daniil <dadyarri@gmail.com>
2024-05-04 11:24:14 +0300

    chore: configure serilog

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,22 @@
 using FastEndpoints;
 using FastEndpoints.Security;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.OpenTelemetry;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.OpenTelemetry(opts => {
+        opts.Endpoint = "http://localhost:4317";
+        opts.Protocol = OtlpProtocol.Grpc;
+        opts.ResourceAttributes = new Dictionary<string, object> {
+            ["service.name"] = "ChocoManager Main API",
+        };
+    })
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Default", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware", LogEventLevel.Verbose)
+    .CreateLogger();
 
 var bld = WebApplication.CreateBuilder();
 bld.Services

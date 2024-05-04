700e3cb82a5f3167039f748e70a3a04c6195404a
Daniil <dadyarri@gmail.com>
2024-05-04 23:38:17 +0300

    refactor: to use header from model binding

--- Api/Domain/Auth/Endpoints/Logout/LogoutEndpoint.cs
+++ Api/Domain/Auth/Endpoints/Logout/LogoutEndpoint.cs
@@ -3,28 +3,28 @@ using Api.Configuration.Swagger;
 using Api.Domain.Auth.Data;
 using Ardalis.Result;
 using FastEndpoints;
-using Microsoft.Net.Http.Headers;
 
 namespace Api.Domain.Auth.Endpoints.Logout;
 
 public class LogoutEndpoint(IUseCase<AuthorizedLogoutRequest, EmptyResponse> logoutUseCase)
-    : Endpoint<EmptyRequest, Result<EmptyResponse>>
+    : Endpoint<LogoutRequest, Result<EmptyResponse>>
 {
     public override void Configure()
     {
         Post("auth/logout");
         Version(3);
+        Description(x => x.Accepts<LogoutRequest>());
         Options(x => x.WithTags(SwaggerTags.Auth));
         DontThrowIfValidationFails();
     }
 
-    public override async Task<Result<EmptyResponse>> ExecuteAsync(EmptyRequest req, CancellationToken ct)
+    public override async Task<Result<EmptyResponse>> ExecuteAsync(LogoutRequest req, CancellationToken ct)
     {
         return await logoutUseCase.Execute(
             new AuthorizedLogoutRequest
             {
                 User = User,
-                AccessToken = HttpContext.Request.Headers[HeaderNames.Authorization].First()!.Split(" ")[1].Trim()
+                AccessToken = req.BearerToken.Split(" ")[1].Trim()
             },
             ct
         );

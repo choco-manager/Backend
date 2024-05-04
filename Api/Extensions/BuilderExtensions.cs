9eff33c1c9949d47b16ad0395c7e9bc3dd5b66d2
Daniil <dadyarri@gmail.com>
2024-05-04 12:55:48 +0300

    feat: add automatic registration of use cases in DI

---
+++ Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Api.Common;
+
+namespace Api.Extensions;
+
+public static class BuilderExtensions
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        var types = Assembly.GetCallingAssembly().GetTypes()
+            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IUseCase<,>)));
+
+        foreach (var type in types)
+        {
+            services.AddTransient(typeof(IUseCase<,>), type);
+        }
+
+        return services;
+    }
+}

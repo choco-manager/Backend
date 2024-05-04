6c6c2877dabc627fb9d8f3df3894ba1572030ac9
Daniil <dadyarri@gmail.com>
2024-05-04 18:07:42 +0300

    fix: passing db context options

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Api.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
 {
-    
+    public virtual DbSet<User> Users => Set<User>();
+    public virtual DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 }

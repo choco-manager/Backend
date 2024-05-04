ab1336082dc0547fac0f1ec366836ad0da723dd9
Daniil <dadyarri@gmail.com>
2024-05-04 12:15:42 +0300

    feat: add ping endpoint

--- Api/Configuration/Swagger/SwaggerTags.cs
+++ Api/Configuration/Swagger/SwaggerTags.cs
@@ -4,4 +4,7 @@ public class SwaggerTags
 {
     [TagDescription("Operations, related to authentication & authorization")]
     public const string Auth = "Auth";
+    
+    [TagDescription("Operations, required for internal purposes, like health checking")]
+    public const string Internal = "Internal";
 }
---
+++ Api/Domain/Internal/Endpoints/PingEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Api.Configuration.Swagger;
+using FastEndpoints;
+
+namespace Api.Domain.Internal.Endpoints;
+
+public class PingEndpoint : Endpoint<EmptyRequest, string>
+{
+    public override void Configure()
+    {
+        Get("/ping");
+        Version(3);
+        Options(x => x.WithTags(SwaggerTags.Internal));
+        AllowAnonymous();
+    }
+
+    public override async Task<string> ExecuteAsync(EmptyRequest req, CancellationToken ct)
+    {
+        return "pong";
+    }
+}

741b5e1e745b21093debf06bae9a013e0bbfaf63
Daniil <dadyarri@gmail.com>
2024-05-05 19:17:05 +0300

    feat: add mapper for product model

---
+++ Api/Domain/Products/Data/ProductMapper.cs
@@ -0,0 +1,12 @@
+﻿using Api.Data.Models;
+using FastEndpoints;
+using Riok.Mapperly.Abstractions;
+
+namespace Api.Domain.Products.Data;
+
+[Mapper]
+public partial class ProductMapper
+{
+    public static partial ProductDto ProductToDto(Product product);
+    private static string TagToString(ProductTag tag) => tag.Title;
+}

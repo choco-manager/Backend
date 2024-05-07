5ca884abc70037706e5ce0dec6c6eb8da4a4da1b
Daniil <dadyarri@gmail.com>
2024-05-07 21:11:14 +0300

    feat: add endpoint to softly delete products

---
+++ Api/Domain/Products/Endpoints/SoftDelete/SoftDeleteProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Api.Common;
+using Api.Configuration.Swagger;
+using Api.Domain.Products.Data;
+using Api.Domain.Products.UseCases;
+using Ardalis.Result;
+using FastEndpoints;
+
+namespace Api.Domain.Products.Endpoints.SoftDelete;
+
+public class SoftDeleteProductEndpoint(SoftDeleteProductUseCase softDeleteProductUseCase)
+    : Endpoint<IdModel, Result<ProductDto>>
+{
+    public override void Configure()
+    {
+        Delete("products/{id:guid}");
+        Version(3);
+        Options(x => x.WithTags(SwaggerTags.Products));
+        DontThrowIfValidationFails();
+    }
+
+    public override async Task<Result<ProductDto>> ExecuteAsync(IdModel req, CancellationToken ct)
+    {
+        return await softDeleteProductUseCase.Execute(req, ct);
+    }
+}
---
+++ Api/Domain/Products/Endpoints/SoftDelete/SoftDeleteProductEndpointSummary.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+
+namespace Api.Domain.Products.Endpoints.SoftDelete;
+
+public class SoftDeleteProductEndpointSummary: Summary<SoftDeleteProductEndpoint>
+{
+    public SoftDeleteProductEndpointSummary()
+    {
+        Summary = "Помечает товар удалённым, не удаляя его насовсем";
+    }
+}

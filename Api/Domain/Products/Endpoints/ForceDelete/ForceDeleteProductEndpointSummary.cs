38b4806d35517f58240fdc10d4958c8ccbb88442
Daniil <dadyarri@gmail.com>
2024-05-07 21:24:35 +0300

    feat: add endpoint to force delete products

---
+++ Api/Domain/Products/Endpoints/ForceDelete/ForceDeleteProductEndpointSummary.cs
@@ -0,0 +1,12 @@
+﻿using Api.Domain.Products.Endpoints.SoftDelete;
+using FastEndpoints;
+
+namespace Api.Domain.Products.Endpoints.ForceDelete;
+
+public class ForceDeleteProductEndpointSummary: Summary<ForceDeleteProductEndpoint>
+{
+    public ForceDeleteProductEndpointSummary()
+    {
+        Summary = "Force deletes product from database";
+    }
+}

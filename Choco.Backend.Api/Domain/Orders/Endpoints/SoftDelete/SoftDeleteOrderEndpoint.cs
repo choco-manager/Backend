92fbe030374e42343dab1c86a15b1a02040943e0
Daniil <dadyarri@gmail.com>
2024-05-22 20:42:52 +0300

    feat: add endpoint to soft delete orders

---
+++ Choco.Backend.Api/Domain/Orders/Endpoints/SoftDelete/SoftDeleteOrderEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Choco.Backend.Api.Common;
+using Choco.Backend.Api.Configuration.Swagger;
+using Choco.Backend.Api.Domain.Orders.UseCases;
+using FastEndpoints;
+
+namespace Choco.Backend.Api.Domain.Orders.Endpoints.SoftDelete;
+
+public class SoftDeleteOrderEndpoint(SoftDeleteOrderUseCase useCase): Endpoint<IdModel, EmptyResponse>
+{
+    public override void Configure()
+    {
+        Delete("orders/{id}");
+        Version(3);
+        Options(x => x.WithTags(SwaggerTags.Orders));
+        DontThrowIfValidationFails();
+    }
+
+    public override async Task<EmptyResponse> ExecuteAsync(IdModel req, CancellationToken ct)
+    {
+        return await useCase.Execute(req, ct);
+    }
+}
---
+++ Choco.Backend.Api/Domain/Orders/Endpoints/SoftDelete/SoftDeleteOrderEndpointSummary.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+
+namespace Choco.Backend.Api.Domain.Orders.Endpoints.SoftDelete;
+
+public class SoftDeleteOrderEndpointSummary : Summary<SoftDeleteOrderEndpoint>
+{
+    public SoftDeleteOrderEndpointSummary()
+    {
+        Summary = "Marks order as deleted, without real deleting";
+    }
+}

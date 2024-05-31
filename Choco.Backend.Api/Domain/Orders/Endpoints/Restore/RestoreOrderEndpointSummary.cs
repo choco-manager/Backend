65166c3c0fa53fe07dc00a4d107e1ac1090dd875
Daniil <dadyarri@gmail.com>
2024-05-31 22:04:59 +0300

    feat: add endpoint to restore soft deleted order

---
+++ Choco.Backend.Api/Domain/Orders/Endpoints/Restore/RestoreOrderEndpointSummary.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+
+namespace Choco.Backend.Api.Domain.Orders.Endpoints.Restore;
+
+public class RestoreOrderEndpointSummary: Summary<RestoreOrderEndpoint>
+{
+    public RestoreOrderEndpointSummary()
+    {
+        Summary = "Restores soft deleted order";
+    }
+}
---
+++ Choco.Backend.Api/Domain/Orders/UseCases/RestoreOrderUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Result;
+using Choco.Backend.Api.Common;
+using Choco.Backend.Api.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Choco.Backend.Api.Domain.Orders.UseCases;
+
+public class RestoreOrderUseCase(AppDbContext db): IUseCase<IdModel, EmptyResponse>
+{
+    public async Task<Result<EmptyResponse>> Execute(IdModel req, CancellationToken ct = default)
+    {
+        var order = await db.Orders
+            .Where(e => e.Id == req.Id)
+            .FirstOrDefaultAsync(ct);
+
+        if (order is null)
+        {
+            return Result.NotFound(nameof(order));
+        }
+
+        order.IdDeleted = false;
+
+        await db.SaveChangesAsync(ct);
+
+        return Result.NoContent();
+    }
+}

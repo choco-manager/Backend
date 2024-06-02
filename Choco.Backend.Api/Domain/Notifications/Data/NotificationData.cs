217c3466af908c94bb5719801c2c05359ce5687b
Daniil <dadyarri@gmail.com>
2024-06-02 14:17:25 +0300

    feat: add use case to create notifications

---
+++ Choco.Backend.Api/Domain/Notifications/Data/NotificationData.cs
@@ -0,0 +1,9 @@
+﻿using Choco.Backend.Api.Data.Models;
+
+namespace Choco.Backend.Api.Domain.Notifications.Data;
+
+public class NotificationData
+{
+    public required Notification Notification { get; set; }
+    public required List<Guid> Recipients { get; set; }
+}
---
+++ Choco.Backend.Api/Domain/Notifications/UseCases/CreateNotificationUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+using Choco.Backend.Api.Common;
+using Choco.Backend.Api.Data;
+using Choco.Backend.Api.Data.Models;
+using Choco.Backend.Api.Domain.Notifications.Data;
+using FastEndpoints;
+
+namespace Choco.Backend.Api.Domain.Notifications.UseCases;
+
+public class CreateNotificationUseCase(AppDbContext db) : IUseCase<NotificationData, EmptyResponse>
+{
+    public async Task<Result<EmptyResponse>> Execute(NotificationData req, CancellationToken ct = default)
+    {
+        await db.Notifications.AddAsync(req.Notification, ct);
+
+        foreach (var recipient in req.Recipients)
+        {
+            await db.NotificationsReadStatus.AddAsync(new NotificationReadStatus
+            {
+                NotificationId = req.Notification.Id,
+                RecipientId = recipient,
+                IsRead = false
+            }, ct);
+        }
+
+        await db.SaveChangesAsync(ct);
+        
+        // TODO: Add firebase calls
+
+        return Result.Success(new EmptyResponse());
+    }
+}

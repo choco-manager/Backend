2b5af244f446019791e54e5c319a1f91d76c7bfc
Daniil <dadyarri@gmail.com>
2024-06-02 16:08:44 +0300

    config: add model for dadata configuration

---
+++ Choco.Backend.Api/Configuration/DadataConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Choco.Backend.Api.Configuration;
+
+public class DadataConfiguration
+{
+    public required string Token { get; set; }
+    public required string Secret { get; set; }
+}

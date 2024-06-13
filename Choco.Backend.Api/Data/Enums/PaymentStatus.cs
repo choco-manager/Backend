e7382eac07051886a2871b6952186242c24b7827
Daniil <dadyarri@gmail.com>
2024-06-13 21:57:05 +0300

    fix: serializing payment status

--- Choco.Backend.Api/Data/Enums/PaymentStatus.cs
+++ Choco.Backend.Api/Data/Enums/PaymentStatus.cs
@@ -1,5 +1,8 @@
-﻿namespace Choco.Backend.Api.Data.Enums;
+﻿using System.Text.Json.Serialization;
 
+namespace Choco.Backend.Api.Data.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum PaymentStatus
 {
     Pending,
